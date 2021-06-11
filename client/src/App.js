8a2fe8610286a4dbf4983cdb265a66e8d57dc516
Mason <masonking@viviscape.com>
2021-06-11 03:43:26 -0400

    added view to create a post

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import PostCreate from './PostCreate';
 
 function App() {
   return (
-    <div>
-      <p>app again</p>
+    <div className="container">
+      <h1>Create Post</h1>
+      <PostCreate></PostCreate>
     </div>
   );
 }
