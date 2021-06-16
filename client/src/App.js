9de87d788779d46088c3c8c96c18d9d3ab6b8c47
Mason <masonking@viviscape.com>
2021-06-16 17:21:04 -0400

    added visuals for comments and post list

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import PostCreate from './PostCreate';
+import PostList from './PostList';
 
-function App() {
+
+const  App = () => {
   return (
     <div className="container">
       <h1>Create Post</h1>
       <PostCreate></PostCreate>
+      <hr/>
+      <h1>Posts</h1>
+      <PostList/>
     </div>
   );
 }
